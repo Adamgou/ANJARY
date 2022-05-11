4c11b55c8d7a5f305eaf436030d1d1174cd25252
ranoa <tsiky@alphamada.com>
2022-05-11 09:45:39 +0300

    FEAT[anj_calendar]:add bool in calendar alarm to set it by default  when created

---
+++ anj_calendar/models/calendar.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Calendar(models.Model):
+    _inherit="calendar.event"
+
+    @api.model
+    def get_alarm(self):
+        return self.env['calendar.alarm'].search([('is_default', '=', True)]).ids
+    
+    alarm_ids = fields.Many2many(default=get_alarm)
+
+class Calendar_alarm(models.Model):
+    _inherit='calendar.alarm'
+
+    is_default = fields.Boolean(f'Rappel d\'alarme par défault')
