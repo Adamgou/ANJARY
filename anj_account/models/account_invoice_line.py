026d4843b053fdcaa770b0d4e456b0ab329de4fd
ranoa <tsiky@alphamada.com>
2022-05-11 08:57:51 +0300

    FEAT[anj_account]:company_dependent=True for record doesn t show in other company

--- anj_account/models/account_invoice_line.py
+++ anj_account/models/account_invoice_line.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class Account_invoice_line(models.Model):
     _inherit='account.move.line'
 
-    unit_price_discounted = fields.Float('Prix unitaire remisé',compute='_get_unit_price_discounted',)
+    unit_price_discounted = fields.Float('Prix unitaire remisé',compute='_get_unit_price_discounted',company_dependent=True)
 
     
     @api.depends('discount')
