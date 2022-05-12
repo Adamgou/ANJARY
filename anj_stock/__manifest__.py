e088db90267579d0cbce33c30e4fdd5992cba10d
ranoa <tsiky@alphamada.com>
2022-05-12 13:38:51 +0300

    FEAT[anj_stock]:hide product based on company

--- anj_stock/__manifest__.py
+++ anj_stock/__manifest__.py
@@ -20,17 +20,19 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','anj_base'],
+    'depends': ['base','anj_base', 'product', 'stock'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/product_template_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
     'license': 'LGPL-3',
+    "uninstall_hook": "uninstall_hook",
 }
