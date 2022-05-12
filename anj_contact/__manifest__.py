b65a36e50fb6e1b44c1f57723606634bb05453df
ranoa <tsiky@alphamada.com>
2022-05-12 15:18:57 +0300

    FEAT[anj_contact]:add additional information

--- anj_contact/__manifest__.py
+++ anj_contact/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','anj_base'],
+    'depends': ['base','anj_base','hr','stock'],
 
     # always loaded
     'data': [
