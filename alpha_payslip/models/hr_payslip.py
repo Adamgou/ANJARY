40c768e80d5a1899182bed3a51b8db7bf94f41ac
ranoa <tsiky@alphamada.com>
2022-05-20 13:12:45 +0300

    add element in report

--- alpha_payslip/models/hr_payslip.py
+++ alpha_payslip/models/hr_payslip.py
@@ -2,14 +2,29 @@
 from datetime import date
 
 from odoo import fields, models, api
+# from odoo.exceptions import UserError, Warning
+# raise UserError(f"La date d'entrée ")
+
 
 
 class Hr_Payslip(models.Model):
     _inherit = 'hr.payslip'
 
-    def get_age(self, birth_date):
+    def get_age(self, birth_date,date_bis):
         if birth_date:
             today = date.today()
-            age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
 
-            return age
+            if date_bis:
+                end_date=date_bis
+                age = end_date.year - birth_date.year 
+                
+
+            else:
+                age = today.year - birth_date.year 
+        age+=1
+        return age
+
+    def get_spent_monthly_hours(self,monthly_hours):
+        if monthly_hours:
+            spent_hours=round(((52*monthly_hours)/12),2)
+            return spent_hours
