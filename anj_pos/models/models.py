088dd7a7487b80585b384b2e64a2bc070a343576
rivo2302 <rivo2302@gmail.com>
2022-07-04 11:33:32 +0300

    ADD [anj_pos] Defaut true on available in POS fields

--- anj_pos/models/models.py
+++ anj_pos/models/models.py
@@ -1,18 +1,11 @@
-# -*- coding: utf-8 -*-
-
-# from odoo import models, fields, api
+from odoo import fields, models, api
 
 
-# class anj_pos(models.Model):
-#     _name = 'anj_pos.anj_pos'
-#     _description = 'anj_pos.anj_pos'
+class ProdcutTemplaceInherit(models.Model):
+    _inherit = "product.template"
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    available_in_pos = fields.Boolean(
+        string="Available in POS",
+        help="Check if you want this product to appear in the Point of Sale.",
+        default=True,
+    )
