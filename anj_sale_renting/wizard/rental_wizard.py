63bf4d6c473a302d19fe6fb8361af0f98bc0c3e4
nexources <nexources@nexources.com>
2022-10-19 10:18:49 +0300

    [FIX] anj_sale_renting: day operation with timedelta

--- anj_sale_renting/wizard/rental_wizard.py
+++ anj_sale_renting/wizard/rental_wizard.py
@@ -21,15 +21,16 @@ class RentalWizard(models.TransientModel):
 
     @api.onchange('location_price_id')
     def onchange_location_price_id(self):
-        if self.location_price_id.id == self.env.ref('anj_sale_renting.journey_price').id:
-            location_interval = LOCATION_INTERVAL.get('journey')
-        elif self.location_price_id.id == self.env.ref('anj_sale_renting.evening_price').id:
-            location_interval = LOCATION_INTERVAL.get('evening')
-        else:
-            location_interval = LOCATION_INTERVAL.get('night')
-        self.pickup_date = self.pickup_date.replace(
-            hour=location_interval[0], minute=0, second=0, microsecond=0)
-        day = (self.pickup_date.date.day +
-               timedelta(days=1 if location_interval[1] <= location_interval[0] else 0)).days
-        self.return_date = self.return_date.replace(
-            day=day, hour=location_interval[1], minute=0, second=0, microsecond=0)
+        if self.location_price_id:
+            if self.location_price_id.id == self.env.ref('anj_sale_renting.journey_price').id:
+                location_interval = LOCATION_INTERVAL.get('journey')
+            elif self.location_price_id.id == self.env.ref('anj_sale_renting.evening_price').id:
+                location_interval = LOCATION_INTERVAL.get('evening')
+            else:
+                location_interval = LOCATION_INTERVAL.get('night')
+            self.pickup_date = self.pickup_date.replace(
+                hour=location_interval[0], minute=0, second=0, microsecond=0)
+            day = (self.pickup_date +
+                timedelta(days=1 if location_interval[1] <= location_interval[0] else 0)).days
+            self.return_date = self.return_date.replace(
+                day=day, hour=location_interval[1], minute=0, second=0, microsecond=0)
