7949d231937f07dfde0219119ffe182a724dfa82
nexources <nexources@nexources.com>
2022-11-11 16:37:42 +0300

    [FIX] anj_subscription

--- anj_subscription/models/sale_subscription.py
+++ anj_subscription/models/sale_subscription.py
@@ -8,9 +8,10 @@ class SaleSubscription(models.Model):
 
     def start_subscription(self):
         res = super(SaleSubscription, self).start_subscription()
-        line_busy = self.env['sale.subscription.line'].search([('analytic_account_id.stage_id', '=', self.env.ref('sale_subscription.sale_subscription_stage_in_progress').id), ('analytic_account_id', '!=', self.id)])
+
+        line_busy = self.env['sale.subscription.line'].search([('analytic_account_id.stage_id', '=', self.env.ref('sale_subscription.sale_subscription_stage_in_progress').id), ('product_id', 'in', self.recurring_invoice_line_ids.mapped('product_id').mapped('id')), ('analytic_account_id', '!=', self.id)])
 
         if line_busy:
-            raise UserError(f"L'immobilier {','.join(line_busy.mapped('name'))} est en cours d abonnement")
+            raise UserError(f"L'immobilier {','.join(line_busy.mapped('name'))} est en cours d'abonnement")
 
         return res
