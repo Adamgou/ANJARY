78f2f8c150eabe6d38365818eae5ca4efc250873
Warren <nexources@nexources.com>
2022-11-25 11:55:04 +0300

    [FEAT] erk_account: journl with no lofo

--- anj_account/models/account_journal.py
+++ anj_account/models/account_journal.py
@@ -5,4 +5,4 @@ from odoo import models, fields
 class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
-    with_logo = fields.Boolean(default=True)
+    no_logo = fields.Boolean(default=False)
