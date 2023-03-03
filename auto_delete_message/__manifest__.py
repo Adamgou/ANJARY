5301908d933932204c765a93315555a9bf147e15
Warren <warren@nexources.com>
2023-03-03 17:14:33 +0300

    [FEAT] auto_delete_message: deletion per res_id

---
+++ auto_delete_message/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Discussion auto delete',
+    'version': '1.5',
+    'category': 'Productivity/Discuss',
+    'sequence': 145,
+    'summary': 'Chat, mail gateway and private channels',
+    'description': "",
+    'website': 'https://www.nexources.com',
+    'depends': ['contacts', 'mail'],
+    'data': [
+        'views/mail_channel_views.xml',
+        'views/res_users_views.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+
+    'license': 'LGPL-3',
+}
