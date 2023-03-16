4f37788d9da34ec21a58313c3611fa0119a82e39
Warren <warren@nexources.com>
2023-03-16 13:57:51 +0300

    [FEAT] auto_delete_message: group

--- auto_delete_message/__manifest__.py
+++ auto_delete_message/__manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 
 {
-    'name': 'Discussion auto delete',
-    'version': '1.5',
-    'category': 'Productivity/Discuss',
-    'sequence': 145,
-    'summary': 'Chat, mail gateway and private channels',
-    'description': "",
-    'website': 'https://www.nexources.com',
-    'depends': ['contacts', 'mail'],
-    'data': [
-        'views/mail_channel_views.xml',
-        'views/res_users_views.xml'
+    "name": "Discussion auto delete",
+    "version": "1.5",
+    "category": "Productivity/Discuss",
+    "sequence": 145,
+    "summary": "Chat, mail gateway and private channels",
+    "description": "",
+    "website": "https://www.nexources.com",
+    "depends": ["contacts", "mail"],
+    "data": [
+        "views/mail_channel_views.xml",
+        "views/res_users_views.xml",
+        "security/res_group.xml",
     ],
-    'demo': [
-    ],
-    'installable': True,
-    'application': True,
-
-    'license': 'LGPL-3',
+    "demo": [],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3",
 }
