9f96504139344d911fed162cd2ba6086beee5bd0
Warren <warren@nexources.com>
2023-03-27 17:09:52 +0300

    [FEAT] anj_mrp: 4 decimal precision on structure and cost

--- anj_mrp/__manifest__.py
+++ anj_mrp/__manifest__.py
@@ -27,6 +27,9 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+
+        'report/mrp_report_bom_structure.xml'
+
     ],
     # only loaded in demonstration mode
     'demo': [
