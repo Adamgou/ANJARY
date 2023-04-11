d28cf221bd6beef5f7604b44d2915b95c4dc46fb
michael <michael@nexources.com>
2023-04-11 10:05:52 +0300

    [Hotfix]pdf report anjary2

--- anj_account/models/product_template.py
+++ anj_account/models/product_template.py
@@ -10,8 +10,9 @@ from odoo.tools import format_amount
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    price_unit_ht = fields.Float('Sale Price HT', required=False, default=0.0, compute='compute_price_HT', store=True)
+    price_unit_ht = fields.Float('Sale Price HT', required=False, default=0.0, compute='compute_price_HT')
 
+    @api.depends('list_price', 'taxes_id')
     def compute_price_HT(self):
         for rec in self:
             tax_rate = rec.taxes_id.filtered(lambda x: x.company_id == self.company_id).amount/100
