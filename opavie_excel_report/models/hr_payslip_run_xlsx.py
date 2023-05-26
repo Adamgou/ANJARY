2d702ff3dc87f608c47b4e61a868ba5e2d2c86d1
vr-baobab <vramilijaona.ext@baobabgroup.com>
2023-05-26 16:21:26 +0300

    UP[report_xlsx,opavie_excel_report]: The module is ready for test

---
+++ opavie_excel_report/models/hr_payslip_run_xlsx.py
@@ -0,0 +1,41 @@
+from odoo import models
+from datetime import date
+
+
+def add_zeros(str_value, length):
+    zeros_to_add = length - len(str_value)
+    str_value = '0' * zeros_to_add + str_value
+    return str_value
+
+
+def float_to_string(float_num):
+    num_str = str(float_num)
+    num_str = num_str.replace('.', '')
+    return num_str
+
+
+class HrPayslipXlsx(models.AbstractModel):
+    _name = 'report.opavie_excel_report.report_opavie_xlsx'
+    _inherit = 'report.report_xlsx.abstract'
+
+    def generate_xlsx_report(self, workbook, data, lines):
+        employer_bank_account = add_zeros(lines.employer_bank_account.acc_number.replace(' ', ''), 23)
+        report_name = 'OPAVI_' + lines.name + '_' + date.today().strftime("%d_%m_%y") + '_' + lines.company_id.name
+        total = 0
+        for value in lines.slip_ids:
+            total = total + value.basic_wage
+        sheet = workbook.add_worksheet('OPAVIE report')
+        sheet.write(0, 0, employer_bank_account)
+        sheet.write(0, 1, add_zeros(str(total).replace('.', ''), 12))
+        # sheet.write(0, 2, '')
+        sheet.write(0, 3, report_name)
+        for index, value in enumerate(lines.slip_ids):
+            employee_bank_account = add_zeros(value.employee_id.bank_account_id.acc_number.replace(' ', ''),
+                                              23) if value.employee_id.bank_account_id.acc_number else " "
+            employee_salary = add_zeros(str(value.basic_wage).replace('.', ''), 12)
+            employee_matricule = add_zeros(str(value.employee_id.matricule).replace(' ', ''), 6) if value.employee_id.matricule else " "
+            employee_name = value.employee_id.name if value.employee_id.name else " "
+            sheet.write(index + 1, 0, employee_bank_account)
+            sheet.write(index + 1, 1, employee_salary)
+            sheet.write(index + 1, 2, employee_matricule)
+            sheet.write(index + 1, 3, employee_name)
