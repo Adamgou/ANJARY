4e5c4d7922844756909145bb292dd6611b77edc0
michael <michael@nexources.com>
2023-08-25 18:54:53 +0300

    [ADD]#1081

--- anj_purchase/__manifest__.py
+++ anj_purchase/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','anj_base'],
+    'depends': ['base','anj_base','purchase','sale_purchase_inter_company_rules'],
 
     # always loaded
     'data': [
