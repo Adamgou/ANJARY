e7bfe751b60a3d7ef05bb3ad7ffecc27ca0224a4
michaelnexources <michael@nexources.com>
2023-11-13 12:24:42 +0300

    [ADD]add 1081

--- anj_purchase_stock/__manifest__.py
+++ anj_purchase_stock/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "anj_purchase",
+    'name': "anj_purchase_stock",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -20,17 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','anj_base','purchase','sale_purchase_inter_company_rules'],
+    'depends': ['purchase_stock'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 'license': 'LGPL-3',
 }
