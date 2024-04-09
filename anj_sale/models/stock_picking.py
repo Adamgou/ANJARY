28b107ab69badbb407241af21fceef38c8b7b931
michaelnexources <michael@nexources.com>
2024-04-09 15:02:32 +0300

    [ADD]#1668+

--- anj_sale/models/stock_picking.py
+++ anj_sale/models/stock_picking.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
-
-
+import calendar
+mois_fr = {
+    1: "janvier",
+    2: "février",
+    3: "mars",
+    4: "avril",
+    5: "mai",
+    6: "juin",
+    7: "juillet",
+    8: "août",
+    9: "septembre",
+    10: "octobre",
+    11: "novembre",
+    12: "décembre"
+}
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     note_client = fields.Char(string="Note", related="sale_id.note_client", store=True)
 
+    scheduled_date_month_year = fields.Char(compute='_get_month_yars')
 
+    def _get_month_yars(self):
+        for rec in self:
+            rec.scheduled_date_month_year = mois_fr[rec.scheduled_date.month] + ' ' + str(rec.scheduled_date.year)
 
