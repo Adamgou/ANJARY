d81b7665b677ad753c986672cbfc52ec1049c7c5
NyAro-Nexources <nyaro@nexources.com>
2024-06-25 15:00:33 +0300

    [ADD]: product subscription

---
+++ anj_product_subscription/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ANJ - Product",
+
+    'summary': "ANJ - Product",
+
+    'author': "Nexources",
+    'website': "https://www.nexources.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'product', 'sale'],
+
+    'data': [
+        'views/product_template_views.xml',
+    ],
+}
+
