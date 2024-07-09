b843dea57b617df729e99c3ddbe125a8ca2f4d84
NyAro-Nexources <nyaro@nexources.com>
2024-07-09 16:03:53 +0300

    [FIX]: discuss

---
+++ anj_discuss/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ANJ - Discuss",
+
+    'summary': "ANJ - Discuss",
+
+    'author': "Nexources",
+    'website': "https://www.nexources.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'mail'],
+
+    'data': [
+        'views/views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'anj_discuss/static/src/js/*.js',
+        ],
+    },
+}
+
