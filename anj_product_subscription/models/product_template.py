86b6d9866bec57ccb3ccd9a2b6a5140bad08024d
NyAro-Nexources <nyaro@nexources.com>
2024-07-16 15:13:43 +0200

    [FIX]: Vue produit abonnement

--- anj_product_subscription/models/product_template.py
+++ anj_product_subscription/models/product_template.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import datetime
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     rented = fields.Boolean(string="Rented", compute="_compute_product_rented")
-    partner_id = fields.Many2one("res.partner", string="Partner", compute="_compute_product_rented")
-    subscription = fields.Many2one("sale.order", string="N° subscription", compute="_compute_product_rented")
+    partner_id = fields.Many2one(
+        "res.partner", string="Partner", compute="_compute_product_rented"
+    )
+    subscription = fields.Many2one(
+        "sale.order", string="N° subscription", compute="_compute_product_rented"
+    )
 
     @api.depends("company_id")
     def _compute_product_rented(self):
@@ -17,10 +22,18 @@ class ProductTemplate(models.Model):
             product.rented = False
             product.partner_id = False
             product.subscription = False
-            if product.env.company.name.lower().startswith('societe immobiliere'):
-                sale_subscription = SaleOrder.search([("is_subscription", "=", True), ('state', 'in', ['sale', 'done'])])
+            if product.env.company.name.lower().startswith("societe immobiliere"):
+                sale_subscription = SaleOrder.search(
+                    [
+                        ("is_subscription", "=", True),
+                        ("state", "in", ["sale", "done"]),
+                        ("subscription_state", "=", "3_progress"),
+                    ]
+                )
                 for order in sale_subscription:
-                    if order.order_line.filtered(lambda l: l.product_id.product_tmpl_id == product):
+                    if order.order_line.filtered(
+                        lambda l: l.product_id.product_tmpl_id == product
+                    ):
                         product.rented = True
                         product.partner_id = order.partner_id
                         product.subscription = order
