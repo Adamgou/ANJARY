2395e5069eab4d2519033dc8b0c800dd3d88b25d
Warren <warren@nexources.com>
2024-09-11 19:46:43 +0300

    ticket#1995 [IMP] anj_security: pos_config_id on res_users view form

--- anj_security/__manifest__.py
+++ anj_security/__manifest__.py
@@ -3,5 +3,8 @@
     "version": "17.0.1.0.0",
     "website": "http://www.nexources.com",
     "license": "AGPL-3",
+    "data": [
+        "views/res_users_views.xml",
+    ],
     "depends": ["anj_pos"],
 }
