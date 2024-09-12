8dafbdc2c15e2cff46423e96558ee68b5ed5080a
Warren <warren@nexources.com>
2024-09-12 10:13:53 +0300

    [IMP] anj_subscription: toggle pos navbar menuitem based on no_backend_access fields

--- anj_security/models/res_users.py
+++ anj_security/models/res_users.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResUsers(models.Model):
@@ -7,3 +7,10 @@ class ResUsers(models.Model):
     _inherit = "res.users"
 
     pos_config_id = fields.Many2one(comodel_name="pos.config")
+    no_backend_access = fields.Boolean(compute="_compute_no_backend_access", store=True)
+
+    @api.depends("pos_config_id")
+    def _compute_no_backend_access(self):
+        """Users with defined pos_config_id should not have access to the backend"""
+        for user in self:
+            user.no_backend_access = bool(user.pos_config_id)
