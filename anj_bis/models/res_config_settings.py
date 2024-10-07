12351eec027b3bb52c0925e63153e8bc9e8f92a9
misanexources <misa@nexources.com>
2024-10-07 10:51:06 +0300

    [REF] delete unuse files

--- anj_bis/models/res_config_settings.py
+++ anj_bis/models/res_config_settings.py
@@ -5,7 +5,7 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    config_logo = fields.Binary(ralted="pos_config_id.config_logo")
-    config_info = fields.Char(ralted="pos_config_id.config_info")
-    config_phone = fields.Char(ralted="pos_config_id.config_phone")
-    config_address = fields.Char(ralted="pos_config_id.config_address")
+    config_logo = fields.Binary(related="pos_config_id.config_logo")
+    config_info = fields.Char(related="pos_config_id.config_info")
+    config_phone = fields.Char(related="pos_config_id.config_phone")
+    config_address = fields.Char(related="pos_config_id.config_address")
