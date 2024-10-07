0cc038b6e8022b75700cc24ecaee3f30875d9a66
NyAro-Nexources <nyaro@nexources.com>
2024-10-07 10:56:51 +0300

    [FIX]: Biskot - Facturation

--- anj_account/models/ir_actions_report.py
+++ anj_account/models/ir_actions_report.py
@@ -10,6 +10,8 @@ class IrActionsReport(models.Model):
     def get_paperformat(self):
         if self.env.company.name.lower().startswith('jara') and self.model == 'account.move':
             return  self.env.ref('anj_account.paperformat_account_a4')
+        elif self.env.company.name.lower().startswith('biskot') and self.model == 'account.move':
+            return  self.env.ref('anj_account.paperformat_account_biskot_a4')
         elif not self.env.company.name.lower().startswith('jara') and self.model == 'account.move':
             return self.env.ref('base.paperformat_euro')
         else:
