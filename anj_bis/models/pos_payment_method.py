e9ecd79c6c49720c98abf604443ccf23a00ef7a9
misanexources <misa@nexources.com>
2024-10-09 09:29:43 +0300

    [REF] add constrains for payment type

--- anj_bis/models/pos_payment_method.py
+++ anj_bis/models/pos_payment_method.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class POsPaymentMethod(models.Model):
@@ -9,3 +10,23 @@ class POsPaymentMethod(models.Model):
     is_spoon_method = fields.Boolean(string="Spoon")
     is_mvola = fields.Boolean(string="Mvola")
     is_cb = fields.Boolean(string="CB")
+
+    @api.constrains("is_mvola", "is_cb")
+    def check_payment_type(self):
+        """
+        Check if mvola and cb payment is unique
+        """
+        mvola_payment = (
+            self.env["pos.payment.method"]
+            .search([("is_mvola", "=", True)])
+            .filtered(lambda l: l.id != self.id)
+        )
+        cb_payment = (
+            self.env["pos.payment.method"]
+            .search([("is_cb", "=", True)])
+            .filtered(lambda l: l.id != self.id)
+        )
+        if mvola_payment and self.is_mvola:
+            raise UserError(_("Mvola payment already exists"))
+        if cb_payment and self.is_cb:
+            raise UserError(_("CB payment already exists"))
